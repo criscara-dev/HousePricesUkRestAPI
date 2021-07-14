8112ff1ccdbcac19200b475f2e01dcfdcb5142b1
Cristian Caratti <ccristian.caratti.xx@gmail.com>
2021-07-14 12:19:45 +0100

    added basic type hinting, and custom JSON type with Union, return class as a type ( on method find_all), adding Black for uniform code formatting, changed all static/instance methods to class method for future flexibility/inheritance

--- UKHouses/__init__.py
+++ UKHouses/__init__.py
@@ -5,102 +5,154 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api, Resource, abort, reqparse, fields, marshal_with
 from flask_rest_paginate import Pagination
-
+from typing import Dict, List, Union
 
 # 1.1 setup sqlite db
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, "houseprices.db")
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'houseprices.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "houseprices.db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 db.Model.metadata.reflect(db.engine)
 Migrate(app, db)
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'mySecretKey'
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.secret_key = "mySecretKey"
 api = Api(app)
+ItemJSON = Dict[str, Union[int, str, float, bool]]
+
 
 # Possible configurations for Paginate
-app.config['PAGINATE_PAGE_SIZE'] = 20
+app.config["PAGINATE_PAGE_SIZE"] = 20
 # app.config['PAGINATE_PAGE_PARAM'] = "pagenumber"
 # app.config['PAGINATE_SIZE_PARAM'] = "pagesize"
 # app.config['PAGINATE_RESOURCE_LINKS_ENABLED'] = False
 # app.config['PAGINATE_PAGINATION_OBJECT_KEY'] = "pagination"
-app.config['PAGINATE_DATA_OBJECT_KEY'] = "houses"
+app.config["PAGINATE_DATA_OBJECT_KEY"] = "houses"
 pagination = Pagination(app, db)
+
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
+
 # MODELS -  a helper that help us ti retrieve HousePricesModel objects from the db - internal repr of an Entity
 class HousePricesModel(db.Model):
-    __table__ = db.Model.metadata.tables['HousePrices']
-    __table_args__ = {'autoload': True}
+    __table__ = db.Model.metadata.tables["HousePrices"]
+    __table_args__ = {"autoload": True}
 
-    def json(self):
-        return {'Id': self.Id, 'code': self.Code, 'price': self.price,
-                'date': (self.date).isoformat(), 'postcode': self.postcode, 'Property Type': self.propType,
-                'New built?': self.newBuild, 'Estate Type': self.estateType, 'House/flat number': self.number,
-                'Street Address': self.street, 'town': self.town, 'district': self.district, 'county': self.county}
+    def json(self) -> ItemJSON:
+        return {
+            "Id": self.Id,
+            "code": self.Code,
+            "price": self.price,
+            "date": self.date.isoformat(),
+            "postcode": self.postcode,
+            "Property Type": self.propType,
+            "New built?": self.newBuild,
+            "Estate Type": self.estateType,
+            "House/flat number": self.number,
+            "Street Address": self.street,
+            "town": self.town,
+            "district": self.district,
+            "county": self.county,
+        }
+
+    @classmethod
+    def find_all(cls) -> List["HousePricesModel"]:
+        return cls.query.all()
 
 
 """
 Resource fields for marshalling
 """
 page_fields = {
-    'Id': fields.String, 'code': fields.String, 'price': fields.String, 'date': fields.String,
-    'postcode': fields.String, 'Property Type': fields.String, 'New built?': fields.String,
-    'Estate Type': fields.String, 'House/flat number': fields.String, 'Street Address': fields.String,
-    'town': fields.String, 'district': fields.String, 'county': fields.String
+    "Id": fields.String,
+    "code": fields.String,
+    "price": fields.String,
+    "date": fields.String,
+    "postcode": fields.String,
+    "Property Type": fields.String,
+    "New built?": fields.String,
+    "Estate Type": fields.String,
+    "House/flat number": fields.String,
+    "Street Address": fields.String,
+    "town": fields.String,
+    "district": fields.String,
+    "county": fields.String,
 }
 
 
 # RESOURCES - external repr of an Entity, it maps endpoints
 class Home(Resource):
-    def get(self):
-        return render_template('index.html'), 200
+    @classmethod
+    def get(cls):
+        return render_template("index.html"), 200
 
 
 class House(Resource):
-    def get(self, Id):
-        houseId = HousePricesModel.query.filter_by(Id=Id).first()
-        if houseId:
-            return houseId.json(), 200
+    @classmethod
+    def get(cls, Id):
+        house_id = HousePricesModel.query.filter_by(Id=Id).first()
+        if house_id:
+            return house_id.json(), 200
         else:
-            return {'Id': None}, 404
+            return {"Id": None}, 404
 
 
 class HouseList(Resource):
     def get(self):
-        # print(request.args)
         args = request.args
-        from_date = args.get('from')
+        from_date = args.get("from")
         to_date = args.get("to")
-        if request.method == 'GET' and 'from' in args is not None and 'to' in args is not None:
+        if (
+            request.method == "GET"
+            and "from" in args is not None
+            and "to" in args is not None
+        ):
             connection = sqlite3.connect(db_path)
             cursor = connection.cursor()
-            query = "SELECT * FROM HousePrices WHERE date BETWEEN ? AND ? ORDER BY date ASC"
-            result = cursor.execute(query, (from_date, to_date,))
+            query = (
+                "SELECT * FROM HousePrices WHERE date BETWEEN ? AND ? ORDER BY date ASC"
+            )
+            result = cursor.execute(
+                query,
+                (
+                    from_date,
+                    to_date,
+                ),
+            )
             housesInDateRange = []
             for row in result:
                 housesInDateRange.append(
-                    {"Id": row[0], "code": row[1], "price": row[2], "date": row[3], "postcode": row[4],
-                     "Property Type": row[5],
-                     "New built?": row[6], "Estate Type": row[7], "House/flat number": row[8], "Street Address": row[9],
-                     "town": row[10]
-                        , "district": row[11], "county": row[12]})
+                    {
+                        "Id": row[0],
+                        "code": row[1],
+                        "price": row[2],
+                        "date": row[3],
+                        "postcode": row[4],
+                        "Property Type": row[5],
+                        "New built?": row[6],
+                        "Estate Type": row[7],
+                        "House/flat number": row[8],
+                        "Street Address": row[9],
+                        "town": row[10],
+                        "district": row[11],
+                        "county": row[12],
+                    }
+                )
             connection.close()
             return pagination.paginate(housesInDateRange, page_fields)
 
         else:
-            houseprices = HousePricesModel.query.all()
+            houseprices = HousePricesModel.find_all()
             return pagination.paginate([hp.json() for hp in houseprices], page_fields)
 
 
-api.add_resource(Home, '/', '/index')
-api.add_resource(HouseList, '/houses')
-api.add_resource(House, '/house/<string:Id>')
-
-
+api.add_resource(Home, "/", "/index")
+api.add_resource(HouseList, "/houses")
+api.add_resource(House, "/house/<string:Id>")
